98f845e644753f64150986286e2c8b4726620887
Braylon Steffen <steffebr@oregonstate.edu>
2021-03-07 19:39:32 -0800

    Q2--Second commit, testing % 4 and !% 100

--- Q2/leapyear.py
+++ Q2/leapyear.py
@@ -1,5 +1,7 @@
 def leapyear(x):
 	if (x % 4 != 0):
 		return False
+	elif (x % 100 == 0):
+		return False
 	else:
 		return True
--- Q2/test_leapyear.py
+++ Q2/test_leapyear.py
@@ -5,6 +5,8 @@ class TestCase(unittest.TestCase):
 	def testLY(self):
 		self.assertEqual(leapyear.leapyear(4), True);
 		self.assertEqual(leapyear.leapyear(5), False);
+		self.assertEqual(leapyear.leapyear(100), False);
+
 
 if __name__ == '__main__':
 	unittest.main()
