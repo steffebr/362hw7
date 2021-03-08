af250e08848c1a62248823818d1572460f13da3b
Braylon Steffen <steffebr@oregonstate.edu>
2021-03-07 19:29:41 -0800

    Q1--Fourth commit, testing Fizz, Buzz, and FizzBuzz

--- Q1/fizzbuzz.py
+++ Q1/fizzbuzz.py
@@ -1,8 +1,10 @@
 def fizzbuzz():
 	for x in range(100):
-		#if x % 3 == 0:
-		#	print("Fizz")
-		if x % 5 == 0:
+		if (x % 3 == 0) and (x % 5 == 0):
+			print("FizzBuzz")
+		elif x % 3 == 0:
+			print("Fizz")
+		elif x % 5 == 0:
 			print("Buzz")
 		else:
 			print(x)
--- Q1/test_fizzbuzz.py
+++ Q1/test_fizzbuzz.py
@@ -8,7 +8,7 @@ class TestCase(unittest.TestCase):
 		output = io.StringIO()
 		sys.stdout = output
 		fizzbuzz.fizzbuzz();
-		self.assertEqual(output.getvalue(), "Buzz\n1\n2\n3\n4\nBuzz\n6\n7\n8\n9\nBuzz\n11\n12\n13\n14\nBuzz\n16\n17\n18\n19\nBuzz\n21\n22\n23\n24\nBuzz\n26\n27\n28\n29\nBuzz\n31\n32\n33\n34\nBuzz\n36\n37\n38\n39\nBuzz\n41\n42\n43\n44\nBuzz\n46\n47\n48\n49\nBuzz\n51\n52\n53\n54\nBuzz\n56\n57\n58\n59\nBuzz\n61\n62\n63\n64\nBuzz\n66\n67\n68\n69\nBuzz\n71\n72\n73\n74\nBuzz\n76\n77\n78\n79\nBuzz\n81\n82\n83\n84\nBuzz\n86\n87\n88\n89\nBuzz\n91\n92\n93\n94\nBuzz\n96\n97\n98\n99\n")
+		self.assertEqual(output.getvalue(), "FizzBuzz\n1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz\n16\n17\nFizz\n19\nBuzz\nFizz\n22\n23\nFizz\nBuzz\n26\nFizz\n28\n29\nFizzBuzz\n31\n32\nFizz\n34\nBuzz\nFizz\n37\n38\nFizz\nBuzz\n41\nFizz\n43\n44\nFizzBuzz\n46\n47\nFizz\n49\nBuzz\nFizz\n52\n53\nFizz\nBuzz\n56\nFizz\n58\n59\nFizzBuzz\n61\n62\nFizz\n64\nBuzz\nFizz\n67\n68\nFizz\nBuzz\n71\nFizz\n73\n74\nFizzBuzz\n76\n77\nFizz\n79\nBuzz\nFizz\n82\n83\nFizz\nBuzz\n86\nFizz\n88\n89\nFizzBuzz\n91\n92\nFizz\n94\nBuzz\nFizz\n97\n98\nFizz\n")
 		sys.stdout = sys.__stdout__
 
 if __name__ == '__main__':
